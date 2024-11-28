0446fe3043cacd653880250189757cfeee2e016b
AJM Saleh <141340344+Abusaleh1234@users.noreply.github.com>
2024-11-28 23:24:26 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc196_c/40318242.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+//Bismillahir Rahmanir Rahim
+//------------------------------------------------------------------------
+#define  ll             long long
+#define  ull            unsigned long long
+#define  nl             "\n"
+#define  cin(arr)       ll arr[n];for(int i=0;i<n;i++)cin>>arr[i]; 
+#define  in(v)          vector<ll>v(n);for(int i=0;i<n;i++)cin>>v[i]; 
+#define  cout(arr)      for(int i=0;i<n;i++)cout<<arr[i]<<" "; 
+#define  out(v)         for(int i=0;i<n;i++)cout<<v[i]<<" "; 
+#define  rep(i,n)       for(int i=0;i<n;i++)
+#define  arrsort(arr)   sort(arr,arr+n);
+#define  vsort(v)       sort(v.begin(),v.end());
+#define  pi             2 * acos (0.0) 
+#define  mod            1000000007
+const int N=1e5+10;     ll dp[N];
+#define  faster         ios_base::sync_with_stdio(0); cin.tie(0); 
+//------------------------------------------------------------------------
+//Bin1234
+
+
+
+int main(){
+    faster;
+    ll n; cin>>n;
+    int cnt=0;
+    string s;
+    for(ll i=1;i<=1000000;i++){
+        s=to_string(i)+to_string(i);
+        ll a=stoll(s);
+        if(a>n)break;
+        else
+        cnt++;
+    }
+    cout<<cnt;
+}
