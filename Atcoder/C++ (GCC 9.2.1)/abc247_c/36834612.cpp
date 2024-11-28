fbbd479d427bdcdf6f358c4376e0bab7194cf10c
AJM Saleh <141340344+Abusaleh1234@users.noreply.github.com>
2024-11-28 23:24:37 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc247_c/36834612.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl "\n"
+// #define rep(i,a,b) for(int i=a;i<b;i++)
+#define pi  2 * acos (0.0) 
+#define faster                        \
+ios_base::sync_with_stdio(false); \
+cin.tie(NULL);
+
+
+int main(){
+    faster;
+
+ int n; cin>>n;
+string s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16;
+s1="1"; s2=s1+" 2 "+s1; s3=s2+" 3 "+s2; s4=s3+" 4 "+s3; s5=s4+" 5 "+s4; s6=s5+" 6 "+s5; s7=s6+" 7 "+s6; s8=s7+" 8 "+s7;s9=s8+" 9 "+s8;
+s10=s9+" 10 "+s9; s11=s10+" 11 "+s10; s12=s11+" 12 "+s11; s13=s12+" 13 "+s12; s14=s13+" 14 "+s13; s15=s14+" 15 "+s14;s16=s15+" 16 "+s15;
+
+if(n==1){cout<<1;return 0;}
+if(n==2){cout<<s2;return 0;}
+if(n==3){cout<<s3;return 0;}
+if(n==4){cout<<s4;return 0;}
+if(n==5){cout<<s5;return 0;}
+if(n==6){cout<<s6;return 0;}
+if(n==7){cout<<s7;return 0;}
+if(n==8){cout<<s8;return 0;}
+if(n==9){cout<<s9;return 0;}
+if(n==10){cout<<s10;return 0;}
+if(n==11){cout<<s11;return 0;}
+if(n==12){cout<<s12;return 0;}
+if(n==13){cout<<s13;return 0;}
+if(n==14){cout<<s14;return 0;}
+if(n==15){cout<<s15;return 0;}
+if(n==16){cout<<s16;return 0;}
+
+
+}
+    
