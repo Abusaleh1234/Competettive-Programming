d6c442e91c53cfce6261b167cc449f98bc9ac1eb
AJM Saleh <141340344+Abusaleh1234@users.noreply.github.com>
2024-11-28 23:24:27 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc205_b/40306995.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+//Bismillahir Rahmanir Rahim
+//------------------------------------------------------------------------
+#define  ll             long long
+#define  ull            unsigned long long
+#define  nl             "\n"
+#define  cin(arr)       ll arr[n];for(int i=0;i<n;i++)cin>>arr[i]; 
+#define  in(v)          vector<ll>v(n);for(int i=0;i<n;i++)cin>>v[i]; 
+#define  cout(arr)      for(int i=0;i<n;i++)cout<<arr[i]<<" "; 
+#define  out(v)         for(int i=0;i<n;i++)cout<<v[i]<<" "; 
+#define  rep(i,n)       for(int i=0;i<n;i++)
+#define  arrsort(arr)   sort(arr,arr+n);
+#define  vsort(v)       sort(v.begin(),v.end());
+#define  pi             2 * acos (0.0) 
+#define  mod            1000000007
+const int N=1e5+10;     ll dp[N];
+#define  faster         ios_base::sync_with_stdio(0); cin.tie(0); 
+//------------------------------------------------------------------------
+//Bin1234
+
+
+
+int main(){
+    faster;
+    int n; cin>>n;
+    
+    int a[n]; set<int>s;
+    rep(i,n){cin>>a[i];s.insert(a[i]);}
+
+    sort(a,a+n);
+    if(s.size()==n && a[n-1]==n)cout<<"Yes";
+    else cout<<"No";
+}
